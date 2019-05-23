479e54e91d9b4b959732db873c3bbe6acf5c229d
Rafael <r.falconi9030@gmail.com>
2019-05-22 19:45:43 -0500

    add swagger documentation

--- app.js
+++ app.js
@@ -5,10 +5,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const api=require ('./routes')  
-
+const api = require('./routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-app.use('/api',api);
+app.use('/api', api);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-module.exports=app
+module.exports = app
